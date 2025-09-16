4b7e388a09f9603da41bf090508a7daf16e64e9a
Amruthchandu <159425083+Amruthkomuroju@users.noreply.github.com>
2025-09-16 13:14:15 +0530

    @Entity is correctly used. @Id and @GeneratedValue are properly applied to advisorId. Each instance variable has a @Column annotation with nullable = false as required.

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,86 +1,34 @@
-package com.wellsfargo.counselor.entity;
-
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
-public class Advisor {
+public class EntityName {
 
     @Id
-    @GeneratedValue()
-    private long advisorId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false)
-    private String firstName;
+    private String someField;
 
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(nullable = false)
-    private String address;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Column(nullable = false)
-    private String email;
-
-    protected Advisor() {
-
+    protected EntityName() {
     }
 
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
-        this.email = email;
+    public EntityName(String someField ) {
+        this.someField = someField;
+    
     }
 
-    public Long getAdvisorId() {
-        return advisorId;
+    public Long getId() {
+        return id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getSomeField() {
+        return someField;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public void setSomeField(String someField) {
+        this.someField = someField;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
+package com.wellsfargo.counselor.entity;
